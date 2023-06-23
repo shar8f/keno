8fe938f1b04bd4465d8d1c25b7835a2d4251255e
shar8f <34315605+shar8f@users.noreply.github.com>
2023-06-23 21:04:19 +0300

    Update keno.js

--- keno.js
+++ keno.js
@@ -12,109 +12,43 @@ const timerLine = gsap.timeline({paused: true})
   .from('.timer-screen', { duration: 2, opacity: 0 })
   .from('.ball-options', { x: '-60vw', duration: 0.5, y: 200, ease: 'bounce.inout', stagger: 0.01 }, "+=0.5")
   .from('.active-yellow, .active-orange', { scale: 1.8, duration: 0.3, stagger: 0.1, ease: 'elastic' }, "-=1")
-  .call(() => {
-    
-    delay = initDelay;
 
-    setTimeout(() => {
-      document.querySelector('.timer-screen').classList.add('hide-label');
-      document.querySelector('.shuffle-screen').classList.remove('hide-label');
-    }, delay-5000);
 
-  })
-  .to({}, {
-    delay: 3,
-    onComplete: function() {
-    shuffleLine.play();
-  }})
+
  
-  console.log("first duration is" + timerLine.duration());
-  console.log(betsTimer);
-
-  const initDelay = (timerLine.duration() + betsTimer) * 1000; // Calculate the delay in milliseconds
+  
 
 // ========================= SHUFFLE TIMELINE ===========================
 
 
-const shuffleLineA = gsap.timeline({paused: true})
-.to('.ball', {
-  x: () => Math.random() > 0.5 ? Math.random() * -800 : Math.random() * 800,
-  y: () => Math.random() * -800,
-  repeat: -1,
-  // stagger: { each: 0.2, from: 'random' },
-  duration: 0.1,
-  yoyo: false,
-  ease: 'power1.out'
-})
+const shuffleWait = betsTimer + 2; // calculate time to wait before loading shuffle screen (allow some time for "bets closed" to flash).
 
-.to('.ball', {
-  x: () => Math.random() > 0.5 ? Math.random() * -800 : Math.random() * 800,
-  y: () => Math.random() * -700,
-  repeat: 1,
-  // stagger: { each: 0.2, from: 'random' },
-  duration: 0.5,
-  yoyo:false,
-  ease: 'power1.out'
-})
+const shuffleLine = gsap.timeline({delay: shuffleWait, paused: true})
+  .call(() => {
+    console.log(" Time to Shuffle!");
+    
+    
+    document.querySelector('.timer-screen').classList.add('hide-label');
+    document.querySelector('.shuffle-screen').classList.remove('hide-label');
 
-.to('.ball', {
-  x: () => Math.random() > 0.5 ? Math.random() * -600 : Math.random() * 800,
-  y: () => Math.random() * -700,
-  repeat: -1,
-  // stagger: { each: 0.2, from: 'random' },
-  duration: 0.2,
-  yoyo: false,
-  ease: 'power1.out'
-})
+    console.log("first duration is" + timerLine.duration());
+    console.log(betsTimer);
+  })
+  .to("#shuffVideo", 0, {opacity: 1 })
+  .to("#shuffVideo", 0, { onComplete: playVideo });
 
-.to('.ball', {
-  x: () => Math.random() > 0.5 ? Math.random() * -500 : Math.random() * 800,
-  y: () => Math.random() * -800,
-  repeat: -1,
-  // stagger: { each: 0.2, from: 'random' },
-  duration: 0.05,
-  yoyo: false,
-  ease: 'power1.out'
-})
-
-.to('.ball', {
-  x: () => Math.random() > 0.5 ? Math.random() * -500 : Math.random() * 800,
-  y: () => Math.random() * -800,
-  repeat: -1,
-  // stagger: { each: 0.2, from: 'random' },
-  duration: 0.4,
-  yoyo: false,
-  ease: 'power1.out'
-}, '-=0.5')
-
-
-const shuffleLineB = gsap.timeline({paused:true})
-  .to('.ball', {
-    x: () => Math.random() > 0.5 ? Math.random() * -500 : Math.random() * 800,
-    y: () => Math.random() * -800,
-    repeat: -1,
-    // stagger: { each: 0.2, from: 'random' },
-    duration: 0.4,
-    yoyo: false,
-    ease: 'power1.out'
-  }, '-=1')
-
-
-  .to('.ball', {
-    x: () => Math.random() > 0.5 ? Math.random() * -500 : Math.random() * 500,
-    y: () => Math.random() * -600,
-    repeat: -1,
-    // stagger: { each: 0.2, from: 'random' },
-    duration: 0.4,
-    yoyo: false,
-    ease: 'power1.out'
-  }, '-=0.5')
-
-
-const shuffleLine = gsap.timeline({paused: true})
-  .add(shuffleLineA.play())
-  .add(shuffleLineB.play())
-
+  // Pause and play the video, and change the button text
+  function playVideo() {
+    // Get the video
+    var video = document.getElementById("shuffVideo");
+    
+    // Add the autoplay attribute to the video element
+    video.setAttribute("autoplay", "");
+  
+    //play the video
+    video.play();
+    }
+  
 
 
 
@@ -122,61 +56,10 @@ const shuffleLine = gsap.timeline({paused: true})
   
   // ============================================== Ball Shuffling
 
-// function ballShuffling() {
-//   // Define the container and ball elements
-// var container = document.getElementById("old-div");
-// var balls = document.getElementsByClassName("ball");
 
-// // Randomize the splash-out position and rotation of the balls
-// function splashOutBalls() {
-//   for (var i = 0; i < balls.length; i++) {
-//     var ball = balls[i];
-//     var radius = container.offsetWidth / 2; // Assuming the container is a perfect circle
 
-//     // Calculate random splash-out distance and rotation
-//     var splashOutDistance = Math.random() * radius * 2; // Random distance within the container circle
-//     var splashOutAngle = Math.random() * 2 * Math.PI;
-//     var splashOutX = splashOutDistance * Math.cos(splashOutAngle);
-//     var splashOutY = splashOutDistance * Math.sin(splashOutAngle);
-//     var rotation = Math.random() * 360;
 
-//     // Apply splash-out position and rotation relative to the current position
-//     gsap.set(ball, { x: "+=" + splashOutX, y: "+=" + splashOutY, rotation: rotation });
-//   }
-// }
 
-// // Animate the balls vigorously
-// function shuffleBalls() {
-//   gsap.fromTo(
-//     balls,
-//     {
-//       x: 0,
-//       y: 0,
-//       rotation: 0,
-//       ease: "power4.out",
-//     },
-//     {
-//       x: "+=200",
-//       y: "+=200",
-//       rotation: "+=720",
-//       duration: 2,
-//       stagger: {
-//         each: 0.1,
-//         repeat: -1,
-//         yoyo: true,
-//       },
-//     }
-//   );
-// }
-
-// // Randomize the initial splash-out position of the balls
-// splashOutBalls();
-
-// // Start the vigorous shuffling animation
-// shuffleBalls();
-
-// } 
-  
 
   // ===============================================Countdown Timer.
 
@@ -243,11 +126,11 @@ setInterval(switchToNextInstruction, 3000);
 // ========================= MAIN TIMELINE =============================
 const mainTimeline = gsap.timeline()
 .add(timerLine.play())
-// .add(shuffleLine.play())
+.add(shuffleLine.play())
 
 mainTimeline.play();
 
 
-console.log(timerLine.duration());
+
 
 
